0d7bf5c8c64b784abc1e7d2c24752c1879a0227f
Wendy Sanarwanto <saintc0d3r@gmail.com>
2016-03-23 15:30:21 +0800

    Added a sample project for Picking & display image from Photo Gallery

---
+++ ImagePickerTutorial/ImagePickerTutorial/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ImagePickerTutorial
+//
+//  Created by Wendy Sanarwanto on 3/23/16.
+//  Copyright © 2016 Mitrais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Initialise imagePicker
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        // imagePicker.sourceType = .Camera
+        // imagePicker.sourceType = .SavedPhotosAlbum
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loadImageButtonTouched(sender: UIButton) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .ScaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
